dcd0153c3901566f3008d514ffae40a2197abeef
Evgeniy Gordinskiy <evgeniy@gordinskiy.com>
2017-09-23 18:56:18 +0300

    Added sorting algorithm.

---
+++ quickSort.php
@@ -0,0 +1,63 @@
+<?php
+qsort($array);
+
+/**
+ *  Determining the maximum and minimum values
+ *
+ * @param $array
+ */
+
+function qsort(&$array) {
+
+$left = 0;
+$right = count($array) - 1;
+mySort($array, $left, $right);
+
+}
+
+/**
+ * Array sorting
+ *
+ * @param $array
+ * @param $left
+ * @param $right
+ */
+
+function mySort(&$array, $left, $right) {
+
+$l = $left;
+$r = $right;
+
+$center = $array[(int)($left + $right) / 2];
+
+do {
+
+while ($array[$r] > $center) { 
+$r--;
+}
+
+while ($array[$l] < $center) { 
+$l++;
+}
+
+if ($l <= $r) {
+
+list($array[$r], $array[$l]) = array($array[$l], $array[$r]);
+
+$l++;
+$r--;
+}
+
+} while ($l <= $r);
+if ($r > $left) {
+
+mySort($array, $left, $r); 
+}
+
+if ($l < $right) {
+
+mySort($array, $l, $right);
+}
+
+}
+
