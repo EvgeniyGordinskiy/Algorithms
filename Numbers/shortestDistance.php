07dbb69216273e2d294a1f666755215daaa91cf7
Evgeniy Gordinskiy <evgeniy@gordinskiy.com>
2017-11-08 20:52:32 +0200

    Added exchangeOfValues, shortestDistance.

---
+++ Numbers/shortestDistance.php
@@ -0,0 +1,22 @@
+<?php
+
+$users = [
+    ['v' => 1,  's' => 7],
+    ['v' => 2, 's' => 1],
+    ['v' => 1,  's' => 3],
+];
+$shortestDistance = array_filter(range(min(array_column($users, 's')), max(array_column($users, 's'))),
+    function($item) use ($users) {
+        $firstTime = abs(($item - $users[0]['s'])) / $users[0]['v'];
+        $same = [];
+        array_shift($users);
+        foreach ($users as $user) {
+            $time = abs(($item - $user['s'])) / $user['v'];
+            if ( $time === $firstTime ) {
+                $same[] = 1;
+            }
+        }
+        return count($users) === count($same);
+    });
+
+var_dump($shortestDistance); // 5
